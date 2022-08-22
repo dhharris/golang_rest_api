9f5419a30dc36c525898ce0a37866f2543d9c490
David Harris <dhharris9@gmail.com>
2022-08-21 22:04:12 -0400

    add more tests

--- web-service/handler/web_service_handler_test.go
+++ web-service/handler/web_service_handler_test.go
@@ -6,17 +6,11 @@ import (
     "testing"
 )
 
-func createTestHandler() WebServiceHandler {
-    options := Options{
-        Storage: data.NewMockStorageHandler(),
-    }
-    handler := NewWebServiceHandler(options)
-
-    return handler
-}
-
 func TestCreateUser(t *testing.T) {
-    handler := createTestHandler()    
+    storage := data.NewMockStorageHandler()
+    handler := NewWebServiceHandler(Options{
+        Storage: storage,
+    })
 
     req := data.NewUserRequest{
         Name: "test",
@@ -24,13 +18,65 @@ func TestCreateUser(t *testing.T) {
 
     user := handler.CreateUser(req)
 
+    // Return value should be the new user
     if user.Name != "test" {
-        t.Fatalf(`CreateUser().Name = %q, want "test"`, user.Name)
+        t.Fatalf(`CreateUser(%v).Name = %q, want "test"`, req, user.Name)
     }
 
     _, err := uuid.Parse(user.ID)
 
     if err != nil {
         t.Fatalf(`Unable to parse UUID %q, %v, error`, user.ID, err)
+    }
+
+    // Test data created
+    have := storage.GetUser(user.ID)
+
+    if have != user {
+        t.Fatalf(`GetUser(%q) = %v, want %v`, user.ID, have, user)
+    }
+}
+
+func TestSaveState(t *testing.T) {
+    storage := data.NewMockStorageHandler()
+    storage.InsertUser(data.User{
+        Name: "test",
+        ID: "uid",
+    })
+    handler := NewWebServiceHandler(Options{
+        Storage: storage,
+    })
+
+    want := data.State{
+        Score: 420,
+        GamesPlayed: 69,
+    }
+
+    handler.SaveState("uid", want)
+
+    have := storage.GetState("uid")
+
+    if want != have {
+        t.Fatalf(`GetState(%q) = %v, want %v`, "uid", have, want)
+    }
+}
+
+func TestLoadState(t *testing.T) {
+
+    storage := data.NewMockStorageHandler()
+    handler := NewWebServiceHandler(Options{
+        Storage: storage,
+    })
+
+    want := data.State{
+        Score: 420,
+        GamesPlayed: 69,
+    }
+    storage.SetState("uid", want)
+
+    have := handler.LoadState("uid")
+
+    if want != have {
+        t.Fatalf(`LoadState(%q) = %v, want %v`, "uid", have, want)
     }
 }
